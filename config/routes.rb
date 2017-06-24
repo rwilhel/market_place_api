c86dd3f85153e82442760059e93572961febf020
rwilhel <rwilhel@gmail.com>
2017-06-24 13:56:55 -0600

    Sets up API headers and settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
+require 'api_constraints'
+
 MarketPlaceApi::Application.routes.draw do
   namespace :api, defaults: { format: :json },
-                              constraints: { subdomain: 'api' }, path: '/'  do
-
+                              constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
+    end
   end
 end
