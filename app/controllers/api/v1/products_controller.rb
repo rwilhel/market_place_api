5fd1a1045eb6541dbe56776f6aa75fba2a6a9f16
rwilhel <rwilhel@gmail.com>
2017-07-01 18:53:50 -0600

    Adds delete-destroy functionality to products model

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ProductsController < ApplicationController
   respond_to :json
-  before_action :authenticate_with_token!, only: [:create]
+  before_action :authenticate_with_token!, only: [:create, :destroy]
 
   def index
     respond_with Product.all
@@ -19,9 +19,15 @@ class Api::V1::ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    product = current_user.products.find(params[:id])
+    product.destroy
+    head 204
+  end
+
   private
 
     def product_params
       params.require(:product).permit(:title, :price, :published)
-    end 
+    end
 end
