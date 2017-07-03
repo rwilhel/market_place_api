5fbeca2fd1435e48def7a28615b06b3ca2f4541d
rwilhel <rwilhel@gmail.com>
2017-07-02 19:56:18 -0600

    Adds scope functionality to filter records by keyword in title

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -12,4 +12,23 @@ describe Product do
 
   it { should_not be_published }
   it { should belong_to :user }
+
+  describe '.filter_by_title' do
+    before(:each) do
+      @product1 = FactoryGirl.create :product, title: "A plasma TV"
+      @product2 = FactoryGirl.create :product, title: "Fastest Laptop"
+      @product3 = FactoryGirl.create :product, title: "CD player"
+      @product4 = FactoryGirl.create :product, title: "LCD TV"
+    end
+
+    context "When a 'TV' title pattern is sent" do
+      it "returns the 2 matching products" do
+        expect(Product.filter_by_title("TV").count).to eq 2
+      end
+
+      it "reeturns the matching products" do
+        expect(Product.filter_by_title("TV").sort).to match_array([@product1, @product4])
+      end
+    end
+  end
 end
