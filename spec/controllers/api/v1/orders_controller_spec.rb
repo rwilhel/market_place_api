e537dcbadab58e546ae5b800361e003fab44f784
rwilhel <rwilhel@gmail.com>
2017-07-03 14:15:05 -0600

    Adds paginagtion for orders index action

--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -15,6 +15,12 @@ describe Api::V1::OrdersController do
       expect(orders_response.count).to eq 4
     end
 
+    it { expect(json_response).to have_key(:meta) }
+    it { expect(json_response[:meta]).to have_key(:pagination) }
+    it { expect(json_response[:meta][:pagination]).to have_key(:per_page) }
+    it { expect(json_response[:meta][:pagination]).to have_key(:total_pages) }
+    it { expect(json_response[:meta][:pagination]).to have_key(:total_objects) }
+
     it { should respond_with 200 }
   end
 
