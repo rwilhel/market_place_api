0346436227c09b507fdf26ac123992eff872e84e
rwilhel <rwilhel@gmail.com>
2017-07-02 22:57:26 -0600

    Adds functionality to decrease product quantity via the placements model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -26,4 +26,17 @@ describe Order do
       expect{@order.set_total!}.to change{@order.total}.from(0).to(185)
     end
   end
+
+  describe '#build_placements_with_products_ids_and_quantities' do
+    before(:each) do
+      product1 = FactoryGirl.create :product, price: 100, quantity: 5
+      product2 = FactoryGirl.create :product, price: 85, quantity: 10
+
+      @product_ids_and_quantities = [[product1.id, 2], [product2.id, 3]]
+    end
+
+    it "builds 2 placements for the order" do
+      expect{order.build_placements_with_products_ids_and_quantities(@product_ids_and_quantities)}.to change{order.placements.size}.from(0).to(2)
+    end
+  end
 end
